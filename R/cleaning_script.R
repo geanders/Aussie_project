e9f1d5337f1fa27a5fb2b05a7d7ecfef43d440ab
Chloe Stenkamp-Strahm <Chloe@Chloes-MacBook-Air.local>
2016-11-02 10:57:21 -0600

    a few changes to r script

--- R/cleaning_script.R
+++ R/cleaning_script.R
@@ -1,7 +1,7 @@
-> setwd("/Users/Chloe/Desktop/")
-> getwd()
-[1] "/Users/Chloe/Desktop"
-> library(dplyr)
+setwd("/Users/Chloe/Desktop/")
+getwd()
+## interested in looking at the data spatially, 
+library(dplyr)
 library(tidyverse)
 aussie_data <- read_csv("mmc1.csv")
 small_aussie <- aussie_data %>% select(-`Family code`, -Location, -Zone, 
