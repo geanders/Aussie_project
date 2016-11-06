5dabaa3b430ac1a355a7735bf8ced611ccbcc0b9
Scott Braley <bralex@colostate.edu>
2016-11-06 11:14:13 -0700

    Tweaked initial "aussie cleanup.R" to add a unique id called entry_num - turns out that sample_id isn't unique, we will have to explore that.
    Tweaked "mapping experiments.R" to rejoin the data sets - the mapping conversion required separating the dataset into pieces, the end result is a new dataframe called geo_aussie_data with 4 fewer observations and lat-long added as new columns.

--- initial aussie cleanup.R
+++ initial aussie cleanup.R
@@ -7,7 +7,7 @@ aussie_data <- read_csv("mmc1.csv")
 # dropping some non-useful columns - leaves 5060 obs. of 53 variables
 clean_aussie_data <- select(aussie_data, -`Database ID`, `Composite count`,
                             -`Family code`, -Datum, -Notes, -Reference, -Geocoding, 
-                            -(contains("err")), -`Dry mass fraction`, -`Mussel age (y)`)
+                            -(contains("err")), -`Mussel age (y)`)
 
 # renaming some columns to simplify R codes
 # deliberately left bare element names capitalized, but shifted isotope names
@@ -20,7 +20,8 @@ clean_aussie_data <- clean_aussie_data %>%
          status = Status, easting = Easting, northing = Northing, zone = Zone,
          u238 = `U-238`, u234 = `U-234`, th230 = `Th-230`, ra226 = `Ra-226`, 
          pb210 = `Pb-210`, po210 = `Po-210`, th232 = `Th-232`, ra228 = `Ra-228`, 
-         th228 = `Th-228`, ac227 = `Ac-227`)
+         th228 = `Th-228`, ac227 = `Ac-227`, k40 = `K-40`, 
+         dry_mass_fraction = `Dry mass fraction`)
 # original dataset uses wildlife_group only for a fine-grained division of 
 # plant and animal samples.  Here I use ecosystem and sample_type to create 
 # entries in wildlife_group for soil and sediment samples, as well as for fruits
@@ -36,3 +37,5 @@ clean_aussie_data$wildlife_group <- as.factor(clean_aussie_data$wildlife_group)
 clean_aussie_data$ecosystem <- as.factor(clean_aussie_data$ecosystem)
 clean_aussie_data$sample_type <- as.factor(clean_aussie_data$sample_type)
 clean_aussie_data$collection_date <- dmy(clean_aussie_data$collection_date)
+# entries in sample_id are not unique, so create an entry number for later use
+clean_aussie_data$entry_num <- 1:nrow(clean_aussie_data)
--- mapping experiments.R
+++ mapping experiments.R
@@ -1,14 +1,16 @@
+# this version takes in clean_aussie_data (5060 obs), filters out the 4 results
+# that don't have locations, converts the UTM coords into lat-long, and ends up
+# with a dataset called geo_aussie_data. 
+library(tidyverse)
 library(rgdal)
 library(sp)
 library(maptools)
 library(ggmap)
 #
 # peeling out the geospatial information for plotting
-geo_data <- select(clean_aussie_data, sample_id, easting, northing, zone)
+#geo_data <- select(clean_aussie_data, sample_id, easting, northing, zone)
 # filter out a few NAs
-geo_data <- subset(geo_data, easting != "" | northing != "")
-#adding some id numbers (may not need these)
-geo_data$entry_num <- 1:nrow(geo_data)
+geo_data <- subset(clean_aussie_data, easting != "" | northing != "")
 # the geo_data is in UTM coordinates, need to convert to lat-long
 # the lat-long conversion requires that the data be split between the UTM
 # zones.  In this case, 52S and 53S.  From here on everything is split.
@@ -20,12 +22,10 @@ coords_53 <- cbind(Easting = as.numeric(as.character(geo_data_53$easting)),
                    Northing = as.numeric(as.character(geo_data_53$northing)))
 # Create the SpatialPointsDataFrame
 spatial_52 <- SpatialPointsDataFrame(coords_52, data = 
-                                         data.frame(geo_data_52$sample_id,
-                                        geo_data_52$entry_num), 
+                                         data.frame(geo_data_52$entry_num), 
                                        proj4string = CRS("+init=epsg:32752"))
 spatial_53 <- SpatialPointsDataFrame(coords_53, data = 
-                                            data.frame(geo_data_53$sample_id,
-                                                       geo_data_53$entry_num), 
+                                            data.frame(geo_data_53$entry_num), 
                                           proj4string = CRS("+init=epsg:32753"))
 
 # Convert to Lat Long
@@ -41,28 +41,23 @@ colnames(spatial_53_ll@coords)[colnames(spatial_53_ll@coords) == "Northing"]<- "
 #
 #  re-merge the data
 geo_data_52_ll <- as.data.frame(spatial_52_ll)
-geo_data_52_ll <- rename(geo_data_52_ll, sample_id = geo_data_52.sample_id)
-geo_data_52_ll <- select(geo_data_52_ll, -geo_data_52.entry_num)
+geo_data_52_ll <- rename(geo_data_52_ll, entry_num = geo_data_52.entry_num)
 geo_data_53_ll <- as.data.frame(spatial_53_ll)
-geo_data_53_ll <- rename(geo_data_53_ll, sample_id = geo_data_53.sample_id)
-geo_data_53_ll <- select(geo_data_53_ll, -geo_data_53.entry_num)
+geo_data_53_ll <- rename(geo_data_53_ll, entry_num = geo_data_53.entry_num)
 
 # recombine
 geo_data_ll <- rbind(geo_data_52_ll, geo_data_53_ll)
+#geo_data_ll$entry_num <- as.character(geo_data_ll$entry_num)
+geo_aussie_data <- left_join(geo_data_ll, clean_aussie_data, by = "entry_num")
 
-#spatial_ll <- spRbind(test_spatial_52_ll, test_spatial_53_ll)
-
-#some plots x and y bounds picked off google maps to give some room
-#y_bounds <- c(-15.5777440, -10.808605)
-#x_bounds <- c(128.915038, 134.184663)
-#plot_52 <- plot(spatial_52_ll, xlim = x_bounds, ylim = y_bounds )
-#plot_53 <- plot(spatial_53_ll, add=TRUE )
-#oz(add = TRUE)
+# drop no-longer-needed geospatial data
+#geo_aussie_data <- select(-easting, -northing, -zone)
 
 northern_terr <- get_map("Darwin", zoom = 7,
                      source = "stamen", maptype = "toner")
 c <- ggmap(northern_terr, extent = "device") +
   geom_point(data = geo_data_ll,
-             aes(x = Longitude, y = Latitude), size = 2, color = "red")
+             aes(x = Longitude, y = Latitude), 
+             size = .5, color = "red", alpha = 0.25)
 c
 
