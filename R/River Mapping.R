db7c93adfd0d86dbb7f0ea7dd334e53efdb9d511
Scott Braley <bralex@colostate.edu>
2016-11-14 11:46:08 -0700

    Minor tweak to River Mapping.R

--- R/River Mapping.R
+++ R/River Mapping.R
@@ -8,14 +8,14 @@ library(foreign)
 library("maptools")
 
 ###For the river lines
-river<-readShapeLines("AUS_water_lines_dcw", proj4string=CRS("+proj=longlat"))
+river<-readShapeLines("River Data/AUS_water_lines_dcw", proj4string=CRS("+proj=longlat"))
 river<-tidy(river)
  river<-filter(river,river$lat < -12 & river$lat > -13.7)
   river<-filter(river,river$long < 133.5 & river$long > 132.2)
   river$id<-as.double(river$id)
   
  #For the River mouth
-rivermouth<-readShapeLines("AUS_water_areas_dcw.shp", proj4string=CRS("+proj=longlat"))
+rivermouth<-readShapeLines("River Data/AUS_water_areas_dcw.shp", proj4string=CRS("+proj=longlat"))
 rivermouth<-tidy(rivermouth)
   rivermouth<-filter(rivermouth, rivermouth$lat < -12 & rivermouth$lat > -12.75)
   rivermouth<-filter(rivermouth, rivermouth$long < 133 & rivermouth$long > 132.4)
