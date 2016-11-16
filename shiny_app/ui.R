eec441f363f26d54c622e73d8062544962218971
Chloe Stenkamp-Strahm <cstenkampstrahm@gmail.com>
2016-11-16 10:52:09 -0700

    shiny thingies

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -3,13 +3,13 @@ shinyUI(fluidPage(
              Presence in the Surrouding Environment"),
            fluidRow(
              column(6,
-                  h4("Where do we see metals and radionuclides?"),
-                  img(src = "Ranger2005.jpg", height = 100, width = 300),
+                  img(src = "Ranger2005.jpg", height = 100, width = 375),
                   h6("Image of Ranger Treatment plant, with mine
                        pit (source www.world-nuclear.org)"),
                     fluidRow(
-                      column(4, checkboxGroupInput("Substrate",
-                             label = h4("Substrate"),
+                      column(12, h4("Where and when do we see metals and radionuclides?")),
+                      column(4, selectInput("Substrate",
+                             label = h5("Sample Type:"),
                             choices = list("Terrestrial Animals" = "Choice 1", 
                             "Terrestrial Plants" = "Choice 2",
                             "Terrestrial soil" = "Choice 3",
@@ -18,22 +18,21 @@ shinyUI(fluidPage(
                               "Mollusc" = "Choice 6",
                         "Freshwater Sediment" = "Choice 7",
                             "Freshwater Plant" = "Choice 8",
-                             "Water" = "Choice 9"),
-                            select = 1)),
-                      column(4, sliderInput("slider1", label = h3("Year of Sampling"),
+                             "Water" = "Choice 9"))),
+                      column(4, sliderInput("slider1", 
+                                            label = h5("Time Period of Sampling:"),
                             min = 1976, max = 2015, value = 1)),
                       column(4, checkboxGroupInput("Substrate",
-                                label = h4("Substrate"),
+                                label = h5("Contaminant:"),
                                 choices = list("Cu" = "Choice 1", 
                                               "As" = "Choice 2",
                                               "Hg" = "Choice 3",
                                               "Pb" = "Choice 4",
                                               "Radionuclides" = "Choice 5"),
                                 select = 1)
-                            )))),
-             fluidRow(
-               column(6,
-                    "Map of Alligator Rivers Sample Sites"))
+                            ))),
+               column(6, position = "right", 
+                    h4("Map of Alligator Rivers Sample Sites")))
                     
            )
     )
