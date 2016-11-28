b912d89b5db4e042078322deb0d0786cdc1225f1
Chloe Stenkamp-Strahm <cstenkampstrahm@gmail.com>
2016-11-28 10:29:48 -0700

    added Preston's new river mapping script, sent via email because DS.store was prohibiting his push

---
+++ River Data/River Mapping Emailed.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library("rgdal")
+library("maptools")
+library("ggplot2")
+library("plyr")
+library(broom)
+library(foreign)
+library("maptools")
+library(leaflet)
+
+###For the river lines
+rivershape<-readShapeLines("River Data/AUS_water_lines_dcw", proj4string=CRS("+proj=longlat"))
+river_map<-rivershape[str_detect(rivershape@data$NAM, "ALLIGATOR"), ]
+
+ #For the River mouth
+rivermouthshape<-readShapeLines("River Data/AUS_water_areas_dcw.shp", proj4string=CRS("+proj=longlat"))
+river_mouthmap<-rivermouthshape[str_detect(rivermouthshape@data$NAM, "ALLIGATOR"), ]
+
+
+
+
+library(leaflet)
+  leaflet()%>%
+    addProviderTiles("Stamen.Watercolor")%>%
+  addPolylines(data=river_map)%>%
+ addPolylines(data=river_mouthmap, fillColor = "#0000FF")%>%
+     addCircleMarkers(data=geo_data_ll, radius=.1)
+  
+###Convert to Data Frame (Takes a very long time)
+river<-tidy(rivershape)
+river<-filter(river,river$lat < -12 & river$lat > -13.7)
+river<-filter(river,river$long < 133.5 & river$long > 132.2)
+
+  
+rivermouth<-tidy(rivermouthshape)
+rivermouth<-filter(rivermouth, rivermouth$lat < -12 & rivermouth$lat > -12.75)
+rivermouth<-filter(rivermouth, rivermouth$long < 133 & rivermouth$long > 132.4)
+
+mape <- leaflet() %>%
+  addProviderTiles("Stamen.Watercolor")
+for(t in unique(test$id)){
+  mape <- mape %>%
+    addPolylines(data = dplyr::filter(test, id == t),
+                 lng = ~ long, lat = ~ lat, color = "#900C3F",
+                 opacity = 0.9)
+}
+mape
+  
+
+  
