f066544283618f8e6865d8512b5d45725b03171c
Scott Braley <bralex@colostate.edu>
2016-11-30 11:22:47 -0700

    Clean up of project folder.

--- R/cleaning_script.R
+++
@@ -1,9 +0,0 @@
-setwd("/Users/Chloe/Desktop/")
-getwd()
-## interested in looking at the data spatially, 
-library(dplyr)
-library(tidyverse)
-aussie_data <- read_csv("mmc1.csv")
-small_aussie <- aussie_data %>% select(-`Family code`, -Location, -Zone, 
-                                       -Datum, -Notes, -Reference, -Geocoding, 
-                                       -`Scientific name`)
