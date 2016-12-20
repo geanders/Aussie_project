ed0096ff35d647b26838b6c997eaa0b7511cf7b5
Brooke Anderson <brooke.anderson@colostate.edu>
2016-12-20 01:09:19 -0700

    Get rid of commented out code in Shiny files

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -36,18 +36,6 @@ shinyServer(function(input, output, session) {
       geo_aussie_shiny %>% filter(sample_type == input$Substrate & 
                                   sample_year == input$slider1)
    }
-
-     #ifelse(input$Substrate == "All", filter(sample_type == input$Substrate &
-                                #sample_year == input$slider1)
-      
-      #if(i == "All"){}
-  #else if(i %in% c("Terrestrial Animals", "Terrestrial Plants", "Terrestrial Soil", 
-   #                                   "Freshwater plant")){filter(sample_type == i)}} %>%
-    
-    #filter(sample_year == input$slider1[1]:input$slider2[2])
-    
-     
-      
   })
   
   
@@ -86,65 +74,4 @@ shinyServer(function(input, output, session) {
   })
   
 })
-
-
-
-
-
-
-
-
-# source("shiny mapping experiments.R")
-# rivershape<-readShapeLines("CopyOfRiver Data/AUS_water_lines_dcw", 
-                           # proj4string=CRS("+proj=longlat"))
-# rivermouthshape<-readShapeLines("CopyOfRiver Data/AUS_water_areas_dcw.shp", 
-                               # proj4string=CRS("+proj=longlat"))
-# from another script file can execute the shiny app by using the runApp 
-# command on the directory name : runApp("shiny_app")
-
-
-# need to run the clean up script to generate clean_aussie_data
-# need to run the river mapping emailed script to generate river and rivermouth data frames
-#load("geo_aussie_data.Rdata")
-#load("river_map.Rdata")
-#load("river_mouthmap.Rdata")
-#geo_aussie_shiny <- geo_aussie_data %>% 
- # mutate(sample_year = year(collection_date)) %>%
-  #select(sample_type, sample_year, Longitude, Latitude, Cu, As, 
-   #   Hg, Pb, radionuclides)
-
-#shinyServer(function(input, output, session) {
- # filteredData <- reactive({
-  #  geo_aussie_shiny[geo_aussie_shiny$sample_type == input$Substrate &
-   #                   geo_aussie_shiny$sample_year >= input$slider1, ]
-  #})
-# points <- filteredData 
-# popup_info <- eventReactive({
-                     # paste0("<b>Copper:</b>  ", 
-                      # points$Cu, "<br/>",
-                      #"<b>Arsenic:</b>  ",
-                      #points$As, "<br/>",
-                       #"<b>Mercury:</b>  ",
-                       #points$Hg, "<br/>",
-                       #"<b>Lead:</b>  ",
-                       #points$Pb, "<br/>",
-                       #"<b>Radionuclides:</b>  ", 
-                       #points$radionuclides)
-#})
-
-#output$RiverMap <- renderLeaflet({
- #   leaflet(geo_aussie_shiny) %>% setView(132.9107, -12.6848, zoom = 7)
-#})
-
-#observe({
- # leafletProxy("RiverMap", data = filteredData()) %>%
- #clearShapes() %>%
-  #  addProviderTiles("Stamen.Watercolor")%>%
-   # addPolylines(data=river_map)%>%
-    #addPolylines(data=river_mouthmap) %>%
-    #addCircleMarkers(data=filteredData())
-#})
-
-#})
-   
 
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -47,36 +47,3 @@ shinyUI(fluidPage(theme=shinytheme("cyborg"),
                     
            ))
     
-
-
-  #sidebarLayout(
-   # sidebarPanel(width = 5,
-    #            h4("Where do we see metals and radionuclides?"),
-     #            img(src = "Ranger2005.jpg", height = 100, width = 250),
-      #           h6("Image of the Ranger Treatment plant, with mine
-       #            pit beyond (source www.world-nuclear.org)"),
-        #        br(),
-         #       checkboxGroupInput("Substrate",
-          #                        label = h4("Substrate"),
-           #     choices = list("Terrestrial Animals" = "Choice 1", 
-            #                   "Terrestrial Plants" = "Choice 2",
-             #                  "Terrestrial soil" = "Choice 3",
-                 #              "Freshwater Animals" = "Choice 4",
-                  #            "Fish" = "Choice 5",
-                   #            "Mollusc" = "Choice 6",
-                    #           "Freshwater Sediment" = "Choice 7",
-                     #         "Freshwater Plant" = "Choice 8",
-                      #         "Water" = "Choice 9"),
-                #select = 1)),
-      #column(4, checkboxGroupInput("Contaminant",
-       #                      label = h5("Contaminant:"),
-        #                     choices = list("Cu" = "Choice 1", 
-         #                                   "As" = "Choice 2",
-          #                                  "Hg" = "Choice 3",
-           #                                 "Pb" = "Choice 4",
-            #                                "Radionuclides" = "Choice 5"),
-             #                select = 1)
-   # mainPanel("Map of Alligator Rivers Area"
-              #plotOutput())
-#  ))
-#))
